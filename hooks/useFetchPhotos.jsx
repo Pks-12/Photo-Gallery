664bf9a0c2d290d27e3359ed409b7182fbeca87a
Pks-12 <153657149+Pks-12@users.noreply.github.com>
2026-03-14 12:13:16 +0530

    Add custom hook for fetching photos from API

---
+++ hooks/useFetchPhotos.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function useFetchPhotos(){
+
+const[photo,setPhoto] = useState([]);
+const[loading,setLoading] = useState(true);
+const[error,setError] = useState(null);
+
+useEffect(()=>   {
+    const fetchPhotos = async() => {
+        try{
+            const response = await fetch("https://picsum.photos/v2/list?limit=30");
+
+            if(!response.ok){
+                throw new Error("Failed to fetch photos");
+            }
+
+            const data = await response.json();
+            setPhoto(data);
+        }
+        catch(err){
+            setError(err.message);
+        }
+        finally{
+            setLoading(false);
+        }
+    };
+
+    fetchPhotos();
+}, []);
+
+return {photo, loading, error};
+
+}
+export default useFetchPhotos;
